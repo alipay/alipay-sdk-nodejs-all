822b2dbf1f05d70e7e08eab29fad284603ef0f81
唐文雍 <tammeny@qq.com>
2020-09-14 16:08:19 +0800

    修改url参数列表

--- lib/alipay.ts
+++ lib/alipay.ts
@@ -154,7 +154,7 @@ class AlipaySdk {
       'app_id', 'method', 'format', 'charset',
       'sign_type', 'sign', 'timestamp', 'version',
       'notify_url', 'return_url', 'auth_token', 'app_auth_token',
-      'appCertSn', 'alipayRootCertSn',
+      'app_cert_sn', 'alipay_root_cert_sn',
     ];
 
     for (const key in params) {
